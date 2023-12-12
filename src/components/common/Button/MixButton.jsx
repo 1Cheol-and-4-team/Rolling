69bbe267be707c33aba1d1a3f49c47792b1d417f
designsoo <kheesoo10@gmail.com>
2023-12-12 23:51:32 +0900

    feat: Implement MixButton component

---
+++ src/components/common/Button/MixButton.jsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames/bind';
+import styles from '@/components/common/Button/Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+export const MixButton = ({
+  variant,
+  size,
+  startIcon = null,
+  endIcon = null,
+  iconSize,
+  iconColor,
+  text,
+  type = 'button',
+  disabled = false,
+  ...props
+}) => {
+  const mixButtonProps = { type, disabled, ...props };
+  const buttonClassName = cx('mix-btn', `btn-${variant}`, `btn--size-${size}`);
+  const iconCalssName = cx(
+    { [startIcon]: startIcon, [endIcon]: endIcon },
+    `ic--size-${iconSize}`,
+    `ic--color-${iconColor}`
+  );
+  return (
+    <button className={buttonClassName} {...mixButtonProps}>
+      <div>
+        {startIcon && <i className={iconCalssName}></i>}
+        <span className={cx(startIcon && 'mg-left', endIcon && 'mg-right')}>
+          {text}
+        </span>
+        {endIcon && <i className={iconCalssName}></i>}
+      </div>
+    </button>
+  );
+};
