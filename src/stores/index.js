e0af29f0be66e9ba5a66b6b6c40d5adf79f4f774
designsoo <kheesoo10@gmail.com>
2023-12-14 20:20:10 +0900

    refactor: Add mock data

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,4 +1,4 @@
-export * from './mock';
 export * from './colorOption';
 export * from './tab';
 export * from './dataType';
+export * from './mock';
---
+++ src/stores/mock.js
@@ -0,0 +1,142 @@
+export const response = {
+  count: 5,
+  next: 'https://rolling-api.vercel.app/2-4/recipients/?limit=3&offset=4&sort=like',
+  previous: 'https://rolling-api.vercel.app/2-4/recipients/?limit=3&sort=like',
+  results: [
+    {
+      id: 14,
+      name: '이윤수',
+      backgroundColor: 'purple',
+      backgroundImageURL: null,
+      createdAt: '2023-11-01T08:01:30.234149Z',
+      messageCount: 0,
+      recentMessages: [],
+      reactionCount: 0,
+      topReactions: [],
+    },
+    {
+      id: 11,
+      name: '손동욱',
+      backgroundColor: 'purple',
+      backgroundImageURL: null,
+      createdAt: '2023-10-31T09:58:37.631384Z',
+      messageCount: 6,
+      recentMessages: [
+        {
+          id: 28,
+          recipientId: 11,
+          sender: 'test user2',
+          profileImageURL:
+            'https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8',
+          relationship: '지인',
+          content: '테스트 목적의 메세지3',
+          font: '나눔명조',
+          createdAt: '2023-10-31T09:58:47.272896Z',
+        },
+        {
+          id: 27,
+          recipientId: 11,
+          sender: 'test user3',
+          profileImageURL:
+            'https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8',
+          relationship: '지인',
+          content: '테스트 목적의 메세지3',
+          font: '나눔명조',
+          createdAt: '2023-10-31T09:58:46.889479Z',
+        },
+        {
+          id: 29,
+          recipientId: 11,
+          sender: 'test user3',
+          profileImageURL:
+            'https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8',
+          relationship: '지인',
+          content: '테스트 목적의 메세지3',
+          font: '나눔명조',
+          createdAt: '2023-10-31T09:58:46.889479Z',
+        },
+        {
+          id: 30,
+          recipientId: 11,
+          sender: 'test user3',
+          profileImageURL:
+            'https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8',
+          relationship: '지인',
+          content: '테스트 목적의 메세지3',
+          font: '나눔명조',
+          createdAt: '2023-10-31T09:58:46.889479Z',
+        },
+        {
+          id: 31,
+          recipientId: 11,
+          sender: 'test user3',
+          profileImageURL:
+            'https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8',
+          relationship: '지인',
+          content: '테스트 목적의 메세지3',
+          font: '나눔명조',
+          createdAt: '2023-10-31T09:58:46.889479Z',
+        },
+        {
+          id: 32,
+          recipientId: 11,
+          sender: 'test user3',
+          profileImageURL:
+            'https://fastly.picsum.photos/id/311/200/200.jpg?hmac=CHiYGYQ3Xpesshw5eYWH7U0Kyl9zMTZLQuRDU4OtyH8',
+          relationship: '지인',
+          content: '테스트 목적의 메세지3',
+          font: '나눔명조',
+          createdAt: '2023-10-31T09:58:46.889479Z',
+        },
+      ],
+      reactionCount: 23,
+      topReactions: [
+        {
+          id: 24,
+          emoji: '😀',
+          count: 15,
+        },
+        {
+          id: 25,
+          emoji: '🥹',
+          count: 8,
+        },
+        {
+          id: 26,
+          emoji: '🥹',
+          count: 8,
+        },
+        {
+          id: 27,
+          emoji: '🥹',
+          count: 8,
+        },
+        {
+          id: 28,
+          emoji: '🥹',
+          count: 8,
+        },
+        {
+          id: 29,
+          emoji: '🥹',
+          count: 8,
+        },
+        {
+          id: 30,
+          emoji: '🥹',
+          count: 8,
+        },
+        {
+          id: 31,
+          emoji: '🥹',
+          count: 8,
+        },
+        {
+          id: 32,
+          emoji: '🥹',
+          count: 8,
+        },
+      ],
+    },
+  ],
+};
