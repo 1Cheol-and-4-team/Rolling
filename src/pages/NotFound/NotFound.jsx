39c18fdd52e59df0a28d5bab94fcb55b31f79abf
designsoo <kheesoo10@gmail.com>
2023-12-21 09:29:22 +0900

    feat: Implement not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,25 @@
+// lib
+import classNames from 'classnames/bind';
+import styles from '@/pages/NotFound/NotFound.module.scss';
+
+import { Button } from '@/components/common/Button';
+import notFound from '@/assets/images/notFound-404.svg';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
 export const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className={cx('not-found')}>
+      <div className={cx('not-found-content')}>
+        <img src={notFound} alt='404' />
+        <h1 className={cx('not-found-title')}>PAGE NOT FOUND</h1>
+        <Link to={'/'}>
+          <Button variant='primary' size={40}>
+            Go Home
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
