194ad2c7e875245f30e06d30172a18f4adb7b29c
designsoo <kheesoo10@gmail.com>
2023-12-21 09:45:26 +0900

    feat: Restructure detail page and Apply media query

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -74,7 +74,7 @@ export const Detail = () => {
   return (
     <div className={cx('detail')}>
       <Helmet>
-        <title> {`${userName}님 페이지 | Rolling`}</title>
+        <title> {`${userName}님 롤링 페이퍼 | Rolling`}</title>
       </Helmet>
 
       <Header />
@@ -87,39 +87,108 @@ export const Detail = () => {
         theme='dark'
       />
       <main className={cx('content-wrapper')}>
-        <aside className={cx('sidebar')}>
-          <div className={cx('sidebar-content')}>
-            <div className={cx('sidebar-header')}>
-              <h2 className={cx('sidebar-title')}>{data.name}</h2>
-              <Count id={id} countData={data} messageData={results} />
-            </div>
-            <Emoji
-              id={id}
-              getEmojiApi={getRecipientApi}
-              getReactionCount={data.reactionCount}
-            />
-            <MemberList id={id} />
-          </div>
-          <Banner />
-        </aside>
-
-        <div className={cx('content')}>
-          <div className={cx('container')}>
-            <div className={cx('content-header')}>
-              <div className={cx('content-header-title')}>
-                <h3>Rolling Paper</h3>
-                <Link to={`/post/${id}/message`}>
-                  <MixButton
-                    variant='outlined'
-                    size={40}
-                    startIcon='ic-plus'
-                    iconSize={12}
-                    iconColor='white'
-                    text='Add'
-                  />
-                </Link>
+        <ul>
+          <li className={cx('md-hidden')}>
+            <aside className={cx('sidebar')}>
+              <div className={cx('sidebar-content')}>
+                <div className={cx('sidebar-header')}>
+                  <h2 className={cx('sidebar-title')}>{data.name}</h2>
+                  <Count id={id} countData={data} messageData={results} />
+                </div>
+                <Emoji
+                  id={id}
+                  getEmojiApi={getRecipientApi}
+                  getReactionCount={data.reactionCount}
+                  isDesktopHide={false}
+                />
+                <MemberList messageData={results} />
               </div>
-              <div className={cx('content-header-filter')}>
+              <Banner />
+            </aside>
+          </li>
+          <li className={cx('md-only')}>
+            <aside className={cx('sidebar')}>
+              <div className={cx('sidebar-content')}>
+                <h2 className={cx('sidebar-title')}>{data.name}</h2>
+                <div className={cx('sidebar-info')}>
+                  <Emoji
+                    id={id}
+                    getEmojiApi={getRecipientApi}
+                    getReactionCount={data.reactionCount}
+                    isDesktopHide={true}
+                  />
+                </div>
+              </div>
+            </aside>
+          </li>
+        </ul>
+
+        <ul>
+          <li className={cx('sm-hidden')}>
+            <div className={cx('content')}>
+              <div className={cx('container')}>
+                <div className={cx('content-header')}>
+                  <div className={cx('content-header-title')}>
+                    <h3>Rolling Paper</h3>
+                    <Link to={`/post/${id}/message`}>
+                      <MixButton
+                        variant='outlined'
+                        size={40}
+                        startIcon='ic-plus'
+                        iconSize={12}
+                        iconColor='white'
+                        text='Add'
+                      />
+                    </Link>
+                  </div>
+                  <div className={cx('content-header-filter')}>
+                    <ul className={cx('tab-list')}>
+                      {SENDER_TAB_LIST.map((item) => (
+                        <li
+                          key={item.id}
+                          onClick={() =>
+                            handleActiveTabClick(item.id, item.option)
+                          }
+                          className={cx(
+                            'tab-list-item',
+                            isActive === item.id && 'tab-active'
+                          )}
+                        >
+                          <button>{item.option}</button>
+                        </li>
+                      ))}
+                    </ul>
+                    <div className={cx('content-header-options')}>
+                      <Dropdown
+                        sortList={SORT_LIST}
+                        size='sm'
+                        onClick={handleValueChange}
+                      />
+                      <IconButton
+                        variant='outlined'
+                        style='square'
+                        icon='ic-share'
+                        iconSize='24'
+                        iconColor='white'
+                        onClick={() => handleCopyClipBoard(url)}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <GridLayout
+                  id={id}
+                  tabName={tabName}
+                  sortOption={sortOption}
+                  backgroundUrl={backgroundUrl}
+                  backgroundColor={backgroundColor}
+                  getMessagesApi={getMessagesApi}
+                />
+              </div>
+            </div>
+          </li>
+          <li className={cx('sm-only')}>
+            <div className={cx('content')}>
+              <div className={cx('container')}>
                 <ul className={cx('tab-list')}>
                   {SENDER_TAB_LIST.map((item) => (
                     <li
@@ -134,33 +203,54 @@ export const Detail = () => {
                     </li>
                   ))}
                 </ul>
-                <div className={cx('content-header-options')}>
-                  <Dropdown
-                    sortList={SORT_LIST}
-                    size='sm'
-                    onClick={handleValueChange}
-                  />
-                  <IconButton
-                    variant='outlined'
-                    style='square'
-                    icon='ic-share'
-                    iconSize='24'
-                    iconColor='white'
-                    onClick={() => handleCopyClipBoard(url)}
-                  />
-                </div>
+                <main className={cx('content-main')}>
+                  <div className={cx('content-main-inner')}>
+                    <div className={cx('content-header')}>
+                      <div className={cx('content-header-title')}>
+                        <h3>Rolling Paper</h3>
+                        <div className={cx('content-header-option')}>
+                          <Link to={`/post/${id}/message`}>
+                            <MixButton
+                              variant='outlined'
+                              size={40}
+                              startIcon='ic-plus'
+                              iconSize={12}
+                              iconColor='white'
+                              text='Add'
+                            />
+                          </Link>
+                          <div className={cx('content-header-options')}>
+                            <Dropdown
+                              sortList={SORT_LIST}
+                              size='sm'
+                              onClick={handleValueChange}
+                            />
+                            <IconButton
+                              variant='outlined'
+                              style='square'
+                              icon='ic-share'
+                              iconSize='24'
+                              iconColor='white'
+                              onClick={() => handleCopyClipBoard(url)}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <GridLayout
+                      id={id}
+                      tabName={tabName}
+                      sortOption={sortOption}
+                      backgroundUrl={backgroundUrl}
+                      backgroundColor={backgroundColor}
+                      getMessagesApi={getMessagesApi}
+                    />
+                  </div>
+                </main>
               </div>
             </div>
-            <GridLayout
-              id={id}
-              tabName={tabName}
-              sortOption={sortOption}
-              backgroundUrl={backgroundUrl}
-              backgroundColor={backgroundColor}
-              getMessagesApi={getMessagesApi}
-            />
-          </div>
-        </div>
+          </li>
+        </ul>
       </main>
     </div>
   );
