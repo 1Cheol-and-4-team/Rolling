8f2d2b43d4094fb9583049695595c4dabe1ada21
Haeun <han.kim0904@gmail.com>
2023-12-24 11:42:20 +0900

    feat: Add background color in profile on Members section

--- src/components/common/SideBar/Member.jsx
+++ src/components/common/SideBar/Member.jsx
@@ -5,10 +5,17 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 export function Member({ image, member, relationship }) {
+  const hexCodeRegex = /[?&]color=([^&]+)/;
+  const match = image.match(hexCodeRegex);
+  const randomColor = match ? match[1] : '#24262B';
+
   return (
     <div className={cx('member')}>
       <div className={cx('member-profile')}>
-        <div className={cx('member-profile-img')}>
+        <div
+          className={cx('member-profile-img')}
+          style={{ '--color': randomColor }}
+        >
           <img src={image} alt='글쓴이 프로필 사진' />
         </div>
         <h1 className={cx('member-profile-name')}>
